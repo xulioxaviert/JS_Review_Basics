37b5efa2905a073915e5f568c7d38122302f8ad0
Xulio Xaviert Rojas Teixeira <xulioxaviert@gmail.com>
2022-03-03 22:09:08 +0100

    Iteración #1: Mix for e includes

---
+++ movies.js
@@ -0,0 +1,29 @@
+// Iteración #1: Mix for e includes
+
+// Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes()
+
+const movies = [
+  { title: "Madaraspar", duration: 192, categories: ["comedia", "aventura"] },
+  { title: "Spiderpan", duration: 122, categories: ["aventura", "acción"] },
+  {
+    title: "Solo en Whatsapp",
+    duration: 223,
+    categories: ["comedia", "thriller"],
+  },
+  {
+    title: "El gato con guantes",
+    duration: 111,
+    categories: ["comedia", "aventura", "animación"],
+  },
+];
+
+let categories = [];
+
+for (let i of movies) {
+  for (let j of i.categories) {
+    if (!categories.includes(j)) {
+      categories.push(j);
+    }
+  }
+}
+console.log(categories);
